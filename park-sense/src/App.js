e9a16eb2af8aff30bee63a650ad9378f0f50daf8
Troy D'Amico <troydamico16@gmail.com>
2023-10-01 22:54:58 -0500

    moved docs folder, experimented with primereact

--- park-sense/src/App.js
+++ park-sense/src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api'
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import { Menu } from 'primereact/menu'
 
 function App() {
+  let pages = [
+    {label : 'Home'},
+    {label : 'Availability'},
+    {label : 'Map'}
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PrimeReactProvider>
+      <Menu model={pages}/>
+    </PrimeReactProvider>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
