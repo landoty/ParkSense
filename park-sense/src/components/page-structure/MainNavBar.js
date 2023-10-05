91dccc9b71f0a7b24186eef845de75838a362c49
Troy D'Amico <troydamico16@gmail.com>
2023-10-05 16:17:52 -0500

    Set up routing

--- park-sense/src/components/page-structure/MainNavBar.js
+++ park-sense/src/components/page-structure/MainNavBar.js
@@ -2,24 +2,29 @@ import React from 'react';
 import { PrimeReactProvider } from 'primereact/api'
 import { Menubar } from 'primereact/menubar';
 import { InputText } from 'primereact/inputtext';
+import { useNavigate } from 'react-router-dom'
 import "./../../../node_modules/primereact/resources/themes/lara-light-blue/theme.css";
 import 'primereact/resources/primereact.min.css';
-import logo2 from "../../img/parksense-logo2.png"
+import logo2 from "../../img/parksense-logo2.png";
 
-export default function MainNavBar()
-{
+export default function MainNavBar(){
+    const navigate = useNavigate();
+
     const items = [
         {
             label: 'Home',
             icon: 'pi pi-fw pi-file',
+            command: () => {navigate('/');}
         },
         {
             label: 'Availability',
             icon: 'pi pi-fw pi-pencil',
+            command: () => {navigate('/availability');}
         },
         {
             label: 'Parking Map',
             icon: 'pi pi-fw pi-user',
+            command: () => {navigate('parking-map');}
         }
     ];
 
--- park-sense/src/components/pages/Availability.js
+++ park-sense/src/components/pages/Availability.js
@@ -1,4 +1,5 @@
 import WhatIsParkSense from "../assets/WhatIsParkSense";
 
 export default function Availability(){
+    return(<p>Availability Component</p>)
 }
