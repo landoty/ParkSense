e19e681e297b6cb3c78b7dfdc53c858afa6384a9
Troy D'Amico <troydamico16@gmail.com>
2023-11-17 15:01:30 -0600

    Finished grid setup and populated with primereact Fieldset components

--- park-sense/src/components/pages/availability/LotStatus.js
+++ park-sense/src/components/pages/availability/LotStatus.js
@@ -6,9 +6,7 @@ Date: 10/05/23
 */
 import { useGetSingleLotData } from '../../../hooks/useGetSingleLotData';
 import './styles.css';
-import { Card } from 'primereact/card';
 import { Fieldset } from 'primereact/fieldset';
-import { Panel } from 'primereact/panel';
 
 export default function LotStatus(currentLotId){
     const userData = useGetSingleLotData(currentLotId);
@@ -16,36 +14,44 @@ export default function LotStatus(currentLotId){
         <div>
             {currentLotId.currentLotId ? (
                 <div className='grid-container'>
-                    <div>
-                    {/* <div className='top-middle'> */}
-                        <Card title='Title'>
+                    <center className='header'>
+                        <h1>{userData.capacity - userData.cars} Available Spots</h1>
+                        <p>({userData.cars}/{userData.capacity} occupied)</p>
+                    </center>
+                    <div className='info1'>
+                        <Fieldset legend='Title 1'>
                             <p className='m-0'>
-                                Option 1
-                            </p>
-                        </Card>
-                    </div>
-                    <div>
-                        <Fieldset legend='Title'>
-                            <p className='m-0'>
-                                Option 2
+                                Extra Statistics/Info 1
                             </p>
                         </Fieldset>
                     </div>
-                    <div>
-                        <Panel header='Title'>
+                    <div className='info2'>
+                        <Fieldset legend='Title 2'>
                             <p className='m-0'>
-                                Option 3
+                                Extra Statistics/Info 2
                             </p>
-                        </Panel>
+                        </Fieldset>
+                    </div>
+                    <div className='info3'>
+                        <Fieldset legend='Title 3'>
+                            <p className='m-0'>
+                                Extra Statistics/Info 3
+                            </p>
+                        </Fieldset>
+                    </div>
+                    <div className='info4'>
+                        <Fieldset legend='Title 4'>
+                            <p className='m-0'>
+                                Extra Statistics/Info 4
+                            </p>
+                        </Fieldset>
                     </div>
                 </div> 
-                // <div>
-                //     <p>Lot: {currentLotId.currentLotId}</p>
-                //     <p>Capacity: {userData.capacity}</p>
-                //     <p>Cars: {userData.cars}</p>
-                // </div>
+                // To get lot name: {currentLotId.currentLotId}
             ) : (
-                <p>Please select a parking lot above</p>
+                <center>
+                    <h1>Please select a parking lot above</h1>
+                </center>
             )}
         </div>
     );
