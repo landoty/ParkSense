4f0d47348c7c890cddd48590ab4445fa1ba038a0
user.email <sepehr.noory@outlook.com>
2024-01-25 20:15:37 -0600

    porting to esp32s3 - minor changes in pins

--- microcontroller/src/main.cpp
+++ microcontroller/src/main.cpp
@@ -33,23 +33,23 @@
 #define LED_BUILT_IN 21
 
 // Pin definition for CAMERA_MODEL_AI_THINKER
-#define PWDN_GPIO_NUM     32
+#define PWDN_GPIO_NUM     -1
 #define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
+#define XCLK_GPIO_NUM     10
+#define SIOD_GPIO_NUM     40
+#define SIOC_GPIO_NUM     39
 
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
+#define Y9_GPIO_NUM       48
+#define Y8_GPIO_NUM       11
+#define Y7_GPIO_NUM       12
+#define Y6_GPIO_NUM       14
+#define Y5_GPIO_NUM       16
+#define Y4_GPIO_NUM       18
+#define Y3_GPIO_NUM       17
+#define Y2_GPIO_NUM       15
+#define VSYNC_GPIO_NUM    38
+#define HREF_GPIO_NUM     47
+#define PCLK_GPIO_NUM     13
 
 
 #define SETUP_AP 1 // 1=AP, 0=STA
@@ -175,11 +175,11 @@ void setup() {
     return;
   }
   
-  uint8_t cardType = SD_MMC.cardType();
-  if(cardType == CARD_NONE){
-    Serial.println("No SD Card attached");
-    return;
-  }
+  // uint8_t cardType = SD_MMC.cardType();
+  // if(cardType == CARD_NONE){
+  //   Serial.println("No SD Card attached");
+  //   return;
+  // }
 
   // Wi-Fi connection
   // SETUP_AP allows MCU to function as an access point for local testing
